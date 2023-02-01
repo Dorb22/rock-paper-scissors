8abcd2a43c4799abb528e8c88895a9c69b62f572
Dorb22 <orbe.dan22@gmail.com>
2023-02-01 17:44:35 +1100

    Make playRound function

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 function getComputerChoice() {
-    let com = Math.floor(Math.random() * 3 + 1);
+    const com = Math.floor(Math.random() * 3 + 1);
     if (com === 1){
-        return 'rock';
+        return 'Rock';
     }
     else if (com === 2){
-        return 'paper';
+        return 'Paper';
     }
     else {
-        return 'scissors';
+        return 'Scissors';
     }
 }
 
-let player = prompt('Type rock, paper or scissors.', 'rock');
-let playerChoice = player.toLowerCase();
-if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors'){
-    console.log(playerChoice);
+function playRound (playerSelection, computerSelection){
+    const player = prompt ('Type rock, paper or scissors.', 'rock');
+    const playerSelect = player.slice(0,1);
+    playerSelection = playerSelect.toUpperCase() + player.slice(1);
+    computerSelection = getComputerChoice();
+    let winner;
+    if (playerSelection === computerSelection){
+        console.log(`It's a draw! ${playerSelection} vs ${computerSelection}.`);
+    }
+    else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
+        console.log('You win! Paper beats Rock.');
+    }
+    else if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
+        console.log('You win! Rock beats Scissors.');
+    }
+    else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
+        console.log('You win! Scissors beats Paper.');
+    }
+    else console.log (`You lose! ${computerSelection} beats ${playerSelection}`);
 }
-else {
-    console.log ('Please type your answer correctly.');
-}
+
+playRound();
+
     
 
