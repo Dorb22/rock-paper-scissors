596ce73b42cefba8b0526db96cb94d48ba517878
Dorb22 <orbe.dan22@gmail.com>
2023-02-01 20:01:12 +1100

    Stop loop when one wins

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore;
+let computerScore;
+
 function getComputerChoice() {
     const com = Math.floor(Math.random() * 3 + 1);
     if (com === 1){
@@ -13,26 +16,40 @@ function getComputerChoice() {
 
 function playRound (playerSelection, computerSelection){
     const player = prompt ('Type rock, paper or scissors.', 'rock');
-    const playerSelect = player.slice(0,1);
-    playerSelection = playerSelect.toUpperCase() + player.slice(1);
+    const slicePlayer = player.slice(0,1);
+    const sliceRest = player.slice(1);
+    playerSelection = slicePlayer.toUpperCase() + sliceRest.toLowerCase();
     computerSelection = getComputerChoice();
-    let winner;
     if (playerSelection === computerSelection){
         console.log(`It's a draw! ${playerSelection} vs ${computerSelection}.`);
     }
     else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
         console.log('You win! Paper beats Rock.');
+        ++playerScore;
     }
     else if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
         console.log('You win! Rock beats Scissors.');
+        ++playerScore;
     }
     else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
         console.log('You win! Scissors beats Paper.');
+        ++playerScore;
     }
-    else console.log (`You lose! ${computerSelection} beats ${playerSelection}`);
+    else {
+        console.log (`You lose! ${computerSelection} beats ${playerSelection}`);
+        ++computerScore;
+    }
+    ;
 }
 
-playRound();
+function game(){
+    for (playerScore = 0, computerScore=0; playerScore < 5, computerScore <5;){
+        playRound();
+        console.log(`Your Score: ${playerScore} Computer Score: ${computerScore}`);
+    }
+}
+
+game();
 
     
 
